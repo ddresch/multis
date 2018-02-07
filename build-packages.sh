be5cb97bf1033866836f18de2817d43b7243d303
archon <hexagon6@fet.li>
2018-02-07 23:27:29 +0100

    fix(build): fix TRAVIS_OS_NAME var

--- build-packages.sh
+++ build-packages.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-[ "${TRAVIS_OS}" = "osx" ] && ( npm run build-mac && mv distribution/Multis{,-${TRAVIS_TAG}}.dmg )
-[ "${TRAVIS_OS}" = "linux" ] && ( npm run build-linux && mv distribution/Multis{,-${TRAVIS_TAG}}.deb )
+[ "${TRAVIS_OS_NAME}" = "osx" ] && ( npm run build-mac && mv distribution/Multis{,-${TRAVIS_TAG}}.dmg )
+[ "${TRAVIS_OS_NAME}" = "linux" ] && ( npm run build-linux && mv distribution/Multis{,-${TRAVIS_TAG}}.deb )
 
 exit 0
